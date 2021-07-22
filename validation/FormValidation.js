cbe44904d2fb83c244702ab5af5bdecb4ff0dc59
Guru127 <gurucharan.gcn@gmail.com>
2021-07-22 21:25:12 +0530

    little styling modification

--- validation/FormValidation.js
+++ validation/FormValidation.js
@@ -3,7 +3,7 @@ const name=document.getElementById('name')
 const password=document.getElementById("password")
 const form=document.getElementById('form')
 const errorElement= document.getElementById('error')
-
+ 
 form.addEventListener('submit',(e)=>{
     let messages =[]
     if(name.value==='' || name.value == null) {
